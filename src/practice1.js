f99977959e724d1a3ffee53a337c8641e6594e0b
SONGYI-chen123 <song1577660501@163.com>
2020-11-14 12:44:57 +0800

    [syc]:finished practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (sourse) =>{
+    let result = [];
+    let {column,data} = sourse;
+
+    data.forEach(element => {
+        let obj = {};
+        element.map((value,i) => {
+            obj[column[i].name] = value;
+        });
+        result.push(obj);
+    });
+    return result;
+
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+    sections.sort((a,b) => {
+        return b.index - a.index;
+    });
+    sections.forEach(element => {
+        items.splice(element.index,0,element.content);
+    });
+    return items;
+}
 export { inject };
